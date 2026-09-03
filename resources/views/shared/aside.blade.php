6f859b8c8a96685080ec76f0e6f492ff63706231
CHR15-V2 <christopheroria2006@gmail.com>
2026-09-03 16:26:07 -0400

    2.Solo juez puede ver expediente

--- resources/views/shared/aside.blade.php
+++ resources/views/shared/aside.blade.php
@@ -92,13 +92,13 @@
                                 <span class="item-name">Denuncias</span>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link @yield('consultaActive')" href="{{ route('consulta.index') }}">
+                                <i class="icon ri-search-eye-line"></i>
+                                <span class="item-name">Consulta</span>
+                            </a>
+                        </li>
                     @endif
-                    <li class="nav-item">
-                        <a class="nav-link @yield('consultaActive')" href="{{ route('consulta.index') }}">
-                            <i class="icon ri-search-eye-line"></i>
-                            <span class="item-name">Consulta</span>
-                        </a>
-                    </li>
 
                     <li>
                         <hr class="hr-horizontal">
